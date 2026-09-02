22d08819dd3e8caf31ca1042b129a2412f63b768
darkside <medini.ali.2000@gmail.com>
2026-09-02 19:29:33 +0100

    feat(db): prisma schema and database client configuration

---
+++ lib/db.ts
@@ -0,0 +1,24 @@
+/* 
+    lib/db.ts
+*/
+import { PrismaClient } from "@/lib/generated/prisma/client";
+
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const adapter = new PrismaNeon({
+  connectionString: process.env.DATABASE_URL,
+});
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    adapter,
+  });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+export default prisma;
